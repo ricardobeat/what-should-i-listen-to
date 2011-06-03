57f84d144ea045aa4b25d95ebe4795a91b42dc59
Ricardo Tomasi <ricardobeat@gmail.com>
2011-06-02 21:18:08 -0300

    quit. compiler doesnt work

---
+++ public/scripts/base.js
@@ -0,0 +1,69 @@
+(function() {
+  var makeNoise;
+  makeNoise = function(opacity) {
+    var canvas, ctx, n, noise, x, y, _ref, _ref2;
+    if (opacity == null) {
+      opacity = 0.2;
+    }
+    if (!document.createElement('canvas').getContext) {
+      return false;
+    }
+    if ((window.localStorage != null) && window.localStorage["noise-" + opacity]) {
+      noise = window.localStorage["noise-" + opacity];
+    } else {
+      canvas = document.createElement('canvas');
+      ctx = canvas.getContext('2d');
+      canvas.width = 45;
+      canvas.height = 45;
+      for (x = 0, _ref = canvas.width; 0 <= _ref ? x < _ref : x > _ref; 0 <= _ref ? x++ : x--) {
+        for (y = 0, _ref2 = canvas.height; 0 <= _ref2 ? y < _ref2 : y > _ref2; 0 <= _ref2 ? y++ : y--) {
+          n = Math.round(Math.random() * 60);
+          ctx.fillStyle = "rgba(" + ([n, n, n, opacity].join(',')) + ")";
+          ctx.fillRect(x, y, 1, 1);
+        }
+      }
+      noise = canvas.toDataURL('image/png');
+      if (window.localStorage != null) {
+        window.localStorage["noise-" + opacity] = noise;
+      }
+    }
+    return document.getElementsByTagName('html')[0].style.backgroundImage = "url(" + noise + ")";
+  };
+  makeNoise(.2);
+  jQuery(function($) {
+    var artist, errorMessage, phrase, phrases, recommend, showRecommendation, username;
+    recommend = $('#recommend');
+    artist = $('#artist');
+    username = $('#username');
+    errorMessage = $('#errorMessage');
+    if (window.localStorage && localStorage.username) {
+      username.val(localStorage.username);
+    }
+    phrases = ["I need to know.", "I have no idea.", "Hmmmmm...", "How about..."];
+    phrase = phrases[~~(Math.random() * phrases.length)];
+    recommend.text(phrase);
+    showRecommendation = function(result) {
+      if (!result) {
+        errorMessage.show();
+      } else {
+        errorMessage.hide();
+      }
+      artist.text(result || 'The Beatles');
+      recommend.text('Try again');
+      return recommend.addClass('used');
+    };
+    return recommend.click(function() {
+      var user;
+      user = username.val();
+      user && window.localStorage && (localStorage.username = user);
+      user || (user = 'superbife');
+      return $.ajax({
+        url: "/recommend/" + user,
+        success: showRecommendation,
+        error: showRecommendation,
+        type: 'GET',
+        dataType: 'text'
+      });
+    });
+  });
+}).call(this);
